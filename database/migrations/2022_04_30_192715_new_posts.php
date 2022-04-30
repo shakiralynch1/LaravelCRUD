9b5f10434c18a7def23592ee07d6207e997eae41
LukePearson86 <D00222875@student.dkit.ie>
2022-04-30 20:35:50 +0100

    new post migration that drops old and adds subtext

--- database/migrations/2022_04_30_192715_new_posts.php
+++ database/migrations/2022_04_30_192715_new_posts.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Posts extends Migration
+class NewPosts extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,8 @@ class Posts extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table){
+        Schema::dropIfExists('posts');
+        Schema::create('new_posts', function (Blueprint $table){
             $table->increments('id');
             $table->string('slug');
             $table->string('title');
