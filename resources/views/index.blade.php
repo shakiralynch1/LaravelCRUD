4b76a4198f4f2a727ef0f02b1168f7fcc64d7574
LukePearson86 <D00222875@student.dkit.ie>
2022-04-30 21:47:34 +0100

    added most recent blog to homepage

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -67,12 +67,37 @@
         </span>
 
         <h2 class="text-4xl font-bold py-10">
-            Recent Posts
+            Most Recent Post
         </h2>
 
-        <p class="m-auto w-4/5 text-gray-500">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque exercitationem saepe enim veritatis, eos temporibus quaerat facere consectetur qui.
-        </p>
+        @foreach ($posts as $post)
+    <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
+        <div>
+            <img src="{{ asset('images/' . $post->image_path) }}" alt="">
+        </div>
+        <div>
+            <h2 class="text-gray-700 font-bold text-5xl pb-4">
+                {{ $post->title }}
+            </h2>
+
+            <span class="text-gray-500">
+                By <span class="font-bold italic text-gray-800">{{ $post->user->name }}</span>, Created on {{ date('jS M Y', strtotime($post->updated_at)) }}
+            </span>
+
+            <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
+                {{ $post->description }}
+            </p>
+            <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
+                {{ $post->sign_off_message }}
+            </p>
+
+            <a href="/blog/{{ $post->slug }}" class="uppercase bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-3xl">
+                Keep Reading
+            </a>
+
+        </div>
+    </div>    
+@endforeach
     </div>
 
     <div class="sm:grid grid-cols-2 w-4/5 m-auto">
