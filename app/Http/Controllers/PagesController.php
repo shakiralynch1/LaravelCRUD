e80622b795b399b2255d7f6758cc43362fc88771
LukePearson86 <D00222875@student.dkit.ie>
2022-04-30 21:38:25 +0100

    added posts to pagescontroller to allow blogs on index

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
+use Cviebrock\EloquentSluggable\Services\SlugService;
 
 class PagesController extends Controller
+
 {
     public function index()
     {
-        return view('index');
+        return view('index')
+            ->with('posts', Post::orderBy('updated_at', 'DESC')->get());
     }
 }
