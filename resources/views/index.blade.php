e5c2991c2e745ba73b82a0c4396294d783321518
shakiralynch1 <92158341+shakiralynch1@users.noreply.github.com>
2022-05-02 15:55:25 +0100

    lol

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,6 +14,8 @@
                 </a>
             </div>
         </div>
+
+        <p>
     </div>
 
     <div class="flex items-center sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
